c4489f2b279fea0e4d446e85a4b69721b84f9049
HerrPython19 <justin.alcorn@maine.edu>
2017-03-09 13:10:47 -0500

    Fixes, and windows support
    Fixed bug in weightFinder where multiple uci engines were opened. Also
    added windows stockfish support, and a best.chromo and worst.chromo
    which save the best/worst overall chromosomes

--- weightFinder.py
+++ weightFinder.py
@@ -5,9 +5,7 @@ chromo example:
 each number float from 0 to 3
 [material,space,protection,advancement,attack,control]
 """
-import random,cPickle,os,ais,chess,chess.uci
-bestchromo = None
-worstchromo = None
+import random,cPickle,os,ais,chess,chess.uci,traceback
 
 def createPopulation(amount):
     chromos = []
@@ -22,7 +20,7 @@ def createPopulation(amount):
 #for each move, these weights try to guess the best move.
 #for each correct best move +1 to subtotal.
 #after every move has been analyzed, return the fitness as correct/total moves
-def fitness(chromo, game):
+def fitness(chromo, game, engine):
     board = chess.Board()
     
     randstart = random.randint(0,len(game)-15)
@@ -46,15 +44,14 @@ def fitness(chromo, game):
     correct = 0
     index = 0
     end = len(game)
-    engine = chess.uci.popen_engine("stockfish/src/./stockfish")
     for move in game:
         engine.position(board)
         my_best = myai.getMove(60)[1]
         sf_best = engine.go(movetime=2000)[0].uci()
         if my_best == sf_best:
             correct += 1
-        if my_best[:2] == sf_best[:2]:
-            correct += .5
+        if my_best[:2] == sf_best[:2] or my_best[2:] == sf_best[2:]:
+            correct += .25
         board.push_uci(move)
         myai.computer = not myai.computer
         index += 1
@@ -102,22 +99,62 @@ def randomGame():
     f.close()
     return gamedata
 
+def getBestLoaded():
+    f = open("best.chromo","rb")
+    data = cPickle.load(f)
+    f.close()
+    return data
+
+def getWorstLoaded():
+    f = open("worst.chromo","rb")
+    data = cPickle.load(f)
+    f.close()
+    return data
+
+def writeBest(chromo):
+    f = open("best.chromo","wb")
+    cPickle.dump(chromo,f)
+    f.close()
+
+def writeWorst(chromo):
+    f = open("worst.chromo","wb")
+    cPickle.dump(chromo,f)
+    f.close()
+
 def do_ga():
     mychromos = createPopulation(50)
     saveable_chromos = mychromos[:]
     mutation_rate = .25
+    bestchromo = None
+    worstchromo = None
+    #mac
+    #engine = chess.uci.popen_engine("stockfish/src/./stockfish")
+    #windows
+    engine = chess.uci.popen_engine("stockfish-8-win/windows/stockfish_8_x64.exe")
     try:
         while True:
             for chromo in mychromos:
-                chromo[1] = fitness(chromo[0],randomGame())
+                chromo[1] = fitness(chromo[0],randomGame(),engine)
 
             #sort based on fitness
             mychromos = sorted(mychromos,key=lambda fit_level:chromo[1])
+            #compare to saved chromos and update
+            bestchromo = mychromos[0]
+            worstchromo = mychromos[len(mychromos)-1]
+            bestloaded = getBestLoaded()
+            worstloaded = getWorstLoaded()
+            if bestchromo[1] > bestloaded[1]:
+                writeBest(bestchromo)
+            if worstchromo[1] < worstloaded[1]:
+                writeWorst(worstchromo)
+            
             mychromos = newPopulation(mychromos)
             saveable_chromos = mychromos[:]
             
     except:
+        traceback.print_exc()
         f = open("all_chromos.ga","wb")
         cPickle.dump(saveable_chromos,f)
         f.close()
 do_ga()
+raw_input()
